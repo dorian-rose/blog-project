3985c3a6683c1cca91c618ef366804c1fb79abb3
dorian-rose <dorianrkelly@gmail.com>
2023-04-05 22:46:27 +0100

    user and admin controllers and ejs complete

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # blog-project
+
+ENDPOINTS: 
+
+To get all entries by one author (by author email):
+[urlBase]entries/:author/:limit/:skip
+
+Obligatory parameters:
+:author - author email
+:limit - results per page 
+:skip - number of results skipped before first shown result 
+
+
+To get all results, from all authors:
+[urlBase]/all-entries/:limit/:skip
+
+Obligatory parameters:
+:limit - results per page 
+:skip - number of results skipped before first shown result 
+
+To get one article by title and author:
+[urlBase]/entry/:title/:author 
+
+Obligatory parameters:
+:title - title of article
+:author - author email
+
+To search all results (all authors) using search term/ key word:
+[urlBase]/search/:search/:limit/:skip
+
+Obligatory parameters:
+:search - search key word 
+:limit - results per page 
+:skip - number of results skipped before first shown result 
--- models/queries.js
+++ models/queries.js
@@ -1,6 +1,6 @@
 const queries = {
-        getAllAuthEntries:
-                `SELECT e.title, e.image, e.date, e.content
+    getAllAuthEntries:
+        `SELECT e.title, e.image, e.date, e.extract, a.email
             FROM entries AS e
             INNER JOIN authors AS a
             ON e.email = a.email
@@ -8,27 +8,27 @@ const queries = {
             ORDER BY e.title
             LIMIT $2
             OFFSET $3`,
-        getAllEntries:
-                `SELECT title, image, date, content
+    getAllEntries:
+        `SELECT title, image, date, content, extract, email
             FROM entries 
             ORDER BY title
             LIMIT $1
             OFFSET $2`,
-        getEntry: `SELECT e.title, e.image, e.date, e.content
+    getEntry: `SELECT e.title, e.image, e.extract, e.date, TO_CHAR(e.date, 'DD/MM/YYYY') AS formatDate, e.content, e.category, a.email
             FROM entries AS e
             INNER JOIN authors AS a
             ON e.email = a.email
             WHERE e.title = $1 and e.email = $2`,
-        createEntry: `INSERT INTO entries(title,content,extract, image, email, category)
+    createEntry: `INSERT INTO entries(title,content,extract, image, email, category)
             VALUES 
             ($1, $2, $3, $4, $5, $6)`,
-        updateEntries: `UPDATE entries
+    updateEntries: `UPDATE entries
             SET title=$1, content=$2, extract=$3, image=$4, email=$5, category=$6
             WHERE title=$7 AND email=$5`,
-        deleteEntries: `DELETE 
+    deleteEntries: `DELETE 
            FROM entries 
            WHERE title=$1 AND email=$2`,
-        searchEntries: `SELECT title, image, date, extract
+    searchEntries: `SELECT title, image, date, extract, email
             FROM entries
             WHERE  title LIKE $1 OR content LIKE $1
             LIMIT $2
